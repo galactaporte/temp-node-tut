78d7ff708ba8f184009a1919765359c31fb5f66c
galactaporte <sharifah3799@gmail.com>
2022-04-29 14:14:46 +0800

    deploy to HEROKU test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer()
 //emits request event
 // subscribe to it/ listen for it / respond to it
 server.on('request', (req, res) => {
-    res.end('Welcome to NODIFY republic')
+    res.end('Welcome to NODIFY republic...and here is the updated version for HEROKU deployment')
 })
 
 server.listen (5000)
