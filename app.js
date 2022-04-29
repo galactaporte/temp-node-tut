a8def432ef22eda332c8e40b092fd166cd4222e6
galactaporte <sharifah3799@gmail.com>
2022-04-29 15:17:46 +0800

    III heroku III

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const server = http.createServer()
 //emits request event
 // subscribe to it/ listen for it / respond to it
 server.on('request', (req, res) => {
-    res.end('Welcome to NODIFY republic...and here is the updated version for HEROKU deployment')
+    res.end('Welcome to NODIFY republic...and here is the updated version for HEROKU deployment (part III)')
 })
 
 server.listen (5000)
